04cd81f1463eed403690be4f223b88a474ff0cd4
Yash Dixit <yashdixitsq@gmail.com>
2022-06-02 23:38:39 +0530

    feat: add error handling middleware for express router

---
+++ src/app/middleware/errors.js
@@ -0,0 +1,43 @@
+const DatabaseError = require('../errors/DatabaseError');
+const NotAuthenticatedError = require('../errors/NotAuthenticatedError');
+const NotFoundError = require('../errors/NotFoundError')
+const ServiceError = require('../errors/ServiceError');
+const BadRequestError = require('../errors/BadRequestError')
+
+
+module.exports = function () {
+    return function (err, req, res, next) {
+        if (err instanceof DatabaseError || err instanceof NotAuthenticatedError || err instanceof NotFoundError || err instanceof ServiceError || err instanceof BadRequestError) {
+            let route = err.route ? err.route : 'Unknown route.';
+            let error = err.error ? err.error.toString() : 'No specific info related to error.';
+            let statusCode = err.statusCode ? err.statusCode : 400;
+            let message = err.message ? err.message : 'Some error occurred.';
+            let name = err.name ? err.name : 'SomeError'
+
+            console.log(`Some error occurred at Route: ${route} with status code: ${statusCode} - message: ${message} - More Info: ${error}`);
+
+            return res.status(statusCode).send({
+                route: route,
+                message: message,
+                statusCode: statusCode,
+                error: error,
+                type: name
+            })
+
+
+        } else {
+
+            /**
+             * trigger alarm here. this was an uncaught error. 
+             */
+
+            return res.status(500).send({
+                message: 'Something went really wrong, please inform yash@attentioun.com',
+                statusCode: 500,
+                error: err,
+                type: 'FATAL ERROR'
+            })
+
+        }
+    }
+}
